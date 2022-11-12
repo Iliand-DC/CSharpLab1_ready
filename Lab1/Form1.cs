4710d5f7b16fe162e5757b88b51546d04fc0972e
Илья Дудкин <iluha805@gmail.com>
2022-11-12 12:57:30 +0300

    Добавьте файлы проекта.

---
+++ Lab1/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        int N, M;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        { // установка размера матрицы и DataGridView
+            int i;
+            int scan = 0;
+            bool start = Int32.TryParse(textBox1.Text, out scan);
+            if (start)
+            {
+                N = Int32.Parse(textBox1.Text);
+                M = N;
+                DataTable matr = new DataTable("matr");
+                DataColumn[] cols = new DataColumn[N];
+                for (i = 0; i < N; i++)
+                {
+                    cols[i] = new DataColumn(i.ToString());
+                    matr.Columns.Add(cols[i]);
+                }
+                for (i = 0; i < N; i++)
+                {
+                    DataRow newRow;
+                    newRow = matr.NewRow();
+                    matr.Rows.Add(newRow);
+                }
+                dataGridView1.DataSource = matr;
+                for (i = 0; i < N; i++)
+                    dataGridView1.Columns[i].Width = 50;
+            }
+        }
+        // обработка матрицы
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Visible = false;
+            MatrMake mt = new MatrMake(N, M);
+            mt.GridToMatrix(dataGridView1);
+            int scan = 0;
+            bool start = Int32.TryParse(textBox1.Text, out scan);
+            if (start)
+            {
+                int delNumber = Convert.ToInt32(deleteBox.Text);
+                if (mt.DelCol(delNumber))
+                {
+                    M = mt.get_col();
+                    N = mt.get_str();
+                    dataGridView1.Visible = true;
+                    mt.MatrixToGrid(dataGridView1);
+                    MessageBox.Show("Колонка " + delNumber + " удалена.");
+                }
+                else
+                {
+                    dataGridView1.Visible = true;
+                    mt.MatrixToGrid(dataGridView1);
+                }
+            }
+        }
+    }
+}
---
+++ Lab1/MatrMake.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data;
+
+namespace Lab1
+{
+    internal class MatrMake
+    {
+        int n_str, //количество строк
+        n_col; //количество столбцов
+        int[,] matrix; // обрабатываемая матрица
+        public MatrMake(int n, int m)
+        {
+            n_str = n;
+            n_col = m;
+            matrix = new int[n, m];
+        }
+
+        public int get_col()
+        {
+            return n_col;
+        }
+
+        public int get_str()
+        {
+            return n_str;
+        }
+        //заполнение матрицы из DataGridView
+        public void GridToMatrix(DataGridView dgv)
+        {
+            DataGridViewCell txtCell;
+            for (int i = 0; i < n_str; i++)
+            {
+                for (int j = 0; j < n_col; j++)
+                {
+                    txtCell = dgv.Rows[i].Cells[j];
+                    //txtCell = dgv.Columns[i].Cells[j];
+                    string s = txtCell.Value.ToString();
+                    if (s == "")
+                        matrix[i, j] = 0;
+                    else
+                        matrix[i, j] = Int32.Parse(s);
+                }
+            }
+        }
+        //вывод матрицы в DataGridView
+        public void MatrixToGrid(DataGridView dgv)
+        {
+            //установка размеров
+            int i;
+            DataTable matr = new DataTable("matr");
+            DataColumn[] cols = new DataColumn[n_col];
+            for (i = 0; i < n_col; i++)
+            {
+                cols[i] = new DataColumn(i.ToString());
+                matr.Columns.Add(cols[i]);
+            }
+            for (i = 0; i < n_str; i++)
+            {
+                DataRow newRow;
+                newRow = matr.NewRow();
+                matr.Rows.Add(newRow);
+            }
+            dgv.DataSource = matr;
+            for (i = 0; i < n_col; i++)
+                dgv.Columns[i].Width = 50;
+            // занесение значений
+            DataGridViewCell txtCell;
+            for (i = 0; i < n_str; i++)
+            {
+                for (int j = 0; j < n_col; j++)
+                {
+                    txtCell = dgv.Rows[i].Cells[j];
+                    txtCell.Value = matrix[i, j].ToString();
+                }
+            }
+        }
+        public bool DelCol(int del_number)
+        {
+            int i, j;
+            bool ok;
+            ok = true;
+            for (j = 0; j < n_col && ok; j++)
+                if (j == del_number)
+                    ok = false;
+            if (!ok)
+            {
+                for (i = 0; i < n_str; i++)
+                    for (j = del_number; j < n_col - 1; j++)
+                        matrix[i, j] = matrix[i, j + 1];
+                n_col--;
+                ok = true;
+            }
+            return ok;
+        }
+    }
+}
